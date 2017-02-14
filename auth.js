76469887087072194ac215d4ae70d1b7c9e0fd12
Kate Humphrey <kate@fullstackacademy.com>
2017-02-14 12:50:38 -0500

    in class additions

--- auth.js
+++ auth.js
@@ -1,11 +1,12 @@
 const router = require('express').Router(),
 	passport = require('passport'),
-	GoogleStrategy = require('passport-google-oauth').OAuth2Strategy,
+	GoogleStrategy = require('passport-google-oauth').OAuth2Strategy, // the GoogleStrategy class
 	User = require('./db').User;
 
-// configuring the strategy (credentials + verification callback)
-passport.use(
-  new GoogleStrategy({
+// configuring the Google strategy(credentials + verification callback)
+  // first parameter is for the first passport.authenticate going to the Provider
+  // second parameters is for the second passport.authenticate coming from the provider
+const theGoogleStrategy = new GoogleStrategy({
     clientID: '904557065956-c3jal5fip47oen1e33p81fe4hu3dtugq.apps.googleusercontent.com',
     clientSecret: 'DDwaBGC6ozKhQVOxZlyKE8U6',
     callbackURL: 'http://127.0.0.1:1337/login/verify'
@@ -20,22 +21,24 @@ passport.use(
       where: {googleId: profile.id},
       defaults: info
     })
-    .spread(function (user) {
-      done(null, user);
+    .then(function ([user, boolCreated]) {
+      done(null, user); //this invokes serializeUser as well as the successRedirect of the passport.authenticate that handles the response from the provider
     })
     .catch(done);
   })
-);
+
+//registering the strategy with passport so we can authenticate by it later
+passport.use(theGoogleStrategy);
 
 // Google authentication and login (notice this is passport.authenticate)
+// If we send in an object with scope as the second parameter passport says ohhh they want me to take the user to talk to the provider
 router.get('/google', passport.authenticate('google', { scope: 'email' }));
 
 // handle the callback after Google has authenticated the user (notice the use of passport.authenticate that takes slightly different variables! AHHHHHH WHYYYYY???!)
+// If we send in an object with successRedirect or failureRedirect it says we are handling something sent from the provider
 router.get('/verify',
-  passport.authenticate('google', { failureRedirect: '/error.html' }),
-  function (req, res) {
-    res.redirect(`/success.html`);
-  }
-);
+  passport.authenticate('google', { successRedirect: '/success.html', failureRedirect: '/error.html' }));
 
 module.exports = router;
+
+
--- index.js
+++ index.js
@@ -1,30 +1,47 @@
 const express = require('express'),
 	app = express(),
-	session = require('express-session'),
+	expressSession = require('express-session'),
 	passport = require('passport'),
 	User = require('./db').User;
 
-app.listen(1337, () => console.log('strangely listenting at port 1337'))
+app.listen(1337, () => console.log('strangely listening at port 1337'))
 
-app.use(session({
-	secret: 'suNNy b3ach3s',
-	resave: false,
-	saveUninitialized: false
+// this MUST be before passport.session() which wraps around express-session
+app.use(expressSession({
+	secret: 'suNNy b3ach3s', // hash used when making the unique IDs
+	resave: false, //don't resave if the session object hasn't changed
+	saveUninitialized: false // do save session object even if we don't add anything to it
 }));
 
+//we should now have req.session
+
+//initializes passport to allow for OAuth strategies
 app.use(passport.initialize());
+
+// passport.session() acts as a middleware to alter the req object and change the 'user' value that is currently the session id (from the client cookie) into the true deserialized user object. It does this through the functions registered below `serializeUser` and `deserializeUser`
 app.use(passport.session());
 
+// This says I have found the user and passed it to done
+	// now we say hey session hold on to this data we will serialize. 
+	// What data we send to done as the second parameter is what will be stored as the session object
 passport.serializeUser((user, done) => {
 	done(null, user.id);
 });
 
+
+// Now a new request is coming in. 
+	// Based on the cookie go lookup this client and the serialized string we stored
+	// What we send into done as the second parameter is what will be stored as the `req.user`
 passport.deserializeUser( (id, done) => {
 	User.findById(id)
-		.then(user => done(null, user))
+		.then(user => {
+			done(null, user)
+		})
 		.catch(done);
 })
 
 app.use('/login', require('./auth.js'));
 
 app.use(express.static('public'));
+
+
